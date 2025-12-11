b7e1e98589537f00892d74d5c6eaae2f20a60b27
lladlam <lladlam@outlook.com>
2025-12-11 15:40:31 +0800

    Update customHtml in footerConfig with new URL

--- src/config.ts
+++ src/config.ts
@@ -389,7 +389,7 @@ export const musicPlayerConfig: MusicPlayerConfig = {
 
 export const footerConfig: FooterConfig = {
 	enable: true, // 是否启用Footer HTML注入功能
-	customHtml: "<img src='https://count.getloli.com/@:name' alt=':name' />", // HTML格式的自定义页脚信息，例如备案号等，默认留空
+	customHtml: "<img src='https://count.getloli.com/@lladlam-blog?name=lladlam-blog&theme=moebooru&padding=7&offset=0&align=top&scale=1&pixelated=1&darkmode=auto' />", // HTML格式的自定义页脚信息，例如备案号等，默认留空
 	// 也可以直接编辑 FooterConfig.html 文件来添加备案号等自定义内容
 	// 注意：若 customHtml 不为空，则使用 customHtml 中的内容；若 customHtml 留空，则使用 FooterConfig.html 文件中的内容
 	// FooterConfig.html 可能会在未来的某个版本弃用
